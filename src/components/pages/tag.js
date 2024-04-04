37e742dbd653f5df619514af57a0520c0f8f098b
AMRUTHAKG2514 <136670491+AMRUTHAKG2514@users.noreply.github.com>
2024-04-04 12:09:01 +0530

    Update tag.js

--- src/components/pages/tag.js
+++ src/components/pages/tag.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState }  from "react";
+import React,{ useEffect, useState } from "react";
 import "./tag.css"
 import { useSearchParams } from 'react-router-dom';
 import { FaPaw } from "react-icons/fa";
@@ -6,7 +6,7 @@ import { FaLocationDot } from "react-icons/fa6";
 
 
 const Tag = () => {
-     const [searchParams] = useSearchParams();
+   const [searchParams] = useSearchParams();
   const tagID = searchParams.get('tagID');
   const [tagData, setTagData] = useState(null);
   const [isDataLoaded, setIsDataLoaded] = useState(false);
@@ -40,38 +40,53 @@ const Tag = () => {
     return <div>Loading...</div>;
   }
   else{
-
-
    return (
-      
+      <div className="Tag">
+      <div className="backgroundheader">
+        
      
       <div className="cardContainer">
         <div className="card">
           <div className="titleContainer">
-            <h3 className="title"><FaPaw/>Hello, my name is {tagData.pet_name|| 'N/A'} Would you help my parents find me?</h3>
+            <h3 className="title"><FaPaw/> Hi! I am {tagData.pet_name|| 'N/A'}, would you help my parents find me?</h3>
           </div>
           <div className="cardContent">
           <div className="image">200 x 200</div>
+           
           <div className="textContainer">
-            <p className="textStyle">Zoop Tag ID: </p>
-            <p className="textStyle">Parent's Name: { tagData.owner_name || 'N/A'}</p>
+          <p className="textStyle">Zoop Tag ID:{tagID} </p>
+            <p className="textStyle">Parent's Name: {tagData.owner_name ||'N/A'}</p>
             <p className="textStyle">Address: {tagData.address || 'N/A'}</p>
             <p className="textStyle">Phone: {tagData.phone ||'N/A'}</p>
-            <p className="textStyle">Email: { tagData.email ||'N/A'}</p>
+            <p className="textStyle">Email: {tagData.email ||  'N/A'}</p>
+           
           </div>
+           </div>
+          <p className="textStyle"></p>
           <p className="textStyle">Other information:</p>
-            <textarea className="textarea" readOnly value={"This is some example of\n multiline text.\nIt can contain multiple lines of information.\nMore data can be shown here."} />
+            <textarea className="textarea" readOnly value={"This is some example of multiline text.\nIt can contain multiple lines of information.\nMore data can be shown here."} />
           <div className="buttonContainer">
               <button className="gpsButton" onClick={() => alert("GPS location sent!")}>
                 <FaLocationDot /> Send GPS location
               </button>
               
             </div>
-            
+             </div>
+        </div>
+      </div>
+   
+      
+      <div className="whoWeAre">
+        <img className="whoWeAreImage" src="./images/dog-image.png" alt=""/> 
+     
+        <div className="whoWeAreTextContainer">
+          <h3>Who We Are</h3>
+          <p className="whoWeAreText1">We Love Your Pet, Just as You Do!</p>
+          <p className="whoWeAreText2">Rem ridi culus pharetra eligendi labore nulla explicabo sem! Modi ipsa, dolores cupiditate elementum molestias dis animi, consequ untur pulvinar, fringilla ducimus facilisis habitant, irure erure? Fringilla metus nulla expli cabo sem!</p>
+          <button className="knowMoreButton">Know More</button>
         </div>
       </div>
       </div>
-      
    
       
    );
